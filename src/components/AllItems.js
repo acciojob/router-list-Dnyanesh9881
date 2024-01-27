2bf933fe0aa3eb727594911979245c48437b49c6
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-01-27 05:09:38 +0000

    addddd

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -8,9 +8,9 @@ const AllItems=()=>{
         <div>
          <h1> Item List</h1>
          <ul>
-            <li><Link to="/items/1">Item1</Link></li>
-            <li><Link to="/items/2">Item2</Link></li>
-            <li><Link to="/items/3">Item3</Link></li>
+            <li><Link to="/items/1">Item 1</Link></li>
+            <li><Link to="/items/2">Item 2</Link></li>
+            <li><Link to="/items/3">Item 3</Link></li>
          </ul>
         </div>
     )
